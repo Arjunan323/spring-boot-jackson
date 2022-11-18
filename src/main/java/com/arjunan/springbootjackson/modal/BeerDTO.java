2c7d9b62c328e6ae3c58d57d7048c43e397d3536
Arjunan323 <uarjunan97@gmail.com>
2022-11-19 00:50:37 +0530

    Json Format annotation. closes #3

--- src/main/java/com/arjunan/springbootjackson/modal/BeerDTO.java
+++ src/main/java/com/arjunan/springbootjackson/modal/BeerDTO.java
@@ -1,5 +1,7 @@
 package com.arjunan.springbootjackson.modal;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,13 +19,17 @@ public class BeerDTO {
 
     private UUID id;
 
+    @JsonProperty("name")
     private String beerName;
 
     private  String beerStyle;
 
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
 }
